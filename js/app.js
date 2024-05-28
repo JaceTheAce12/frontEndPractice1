913f2d1b2c00ed9cbcae125502f88e4b86f89d8c
Jace Randolph <jrandolphacliff@gmail.com>
2024-05-28 14:58:17 -0600

    added font family and started footer

---
+++ js/app.js
@@ -0,0 +1,32 @@
+async function getContent() {
+    try {
+        const response = await fetch('data/content.json');
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        console.log(data);
+
+        data.sections.forEach(element => {
+            console.log(element.title);
+            const content = document.querySelector('.content');
+            content.innerHTML += `
+            <div id="contentContainer">
+                <div id="imgContainer">
+                    <img src="${element.imagePath}" alt="${element.altText}">
+                </div>
+                <div>
+                    <h1>${element.title}</h1>
+                    <p>${element.description}</p>
+                    <a href="#">${element.linkText}</a>
+                </div>
+            </div>
+            `
+        });
+
+    } catch (error) {
+        console.error('Error fetching the content:', error);
+      }
+}
+
+getContent();
